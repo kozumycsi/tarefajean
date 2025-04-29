f1e6a6e39a14df1e664fa29dc2a98ad89d362dc4
aslan jade <167216970+kozumycsi@users.noreply.github.com>
2025-04-29 10:34:08 -0300

    a

--- model/CadastroModel.php
+++ model/CadastroModel.php
@@ -9,8 +9,14 @@ function register($username, $email, $password){
     $sql = "INSERT INTO pessoa (nome, email)VALUES (?, ?)";
     $stmt = $instance->prepare($sql);
     $stmt->execute([$username, $email]);
+
     $idPessoa = $instance->lastInsertId();
     $sql = "INSERT INTO usuario (email, senha, pessoa_id) VALUES (?, ?, ?)";
+    $stmt = $instance->prepare($sql);
+    $stmt->execute([$email, $hashed_password, $idPessoa]);
+
+    $idPessoa = $instance->lastInsertId();
+    $sql = "INSERT INTO code (email, senha, pessoa_id) VALUES (?, ?, ?)";
     $stmt = $instance->prepare($sql);
     $stmt->execute([$email, $hashed_password, $idPessoa]);
  
